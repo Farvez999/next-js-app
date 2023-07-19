247d129523a5980f4a4660bc3d21053a4cea6d17
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-07-20 01:53:12 +0600

    Catch all routes and custom 404 error page and css component library

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <img src='https://media.istockphoto.com/id/1142986365/vector/error-with-glitch-effect-on-screen-error-404-page-not-found-flat-design-modern-vector.jpg?s=612x612&w=0&k=20&c=E0Y1GZsaRrEk-a2eW49WKCjRFd9SS1MitZ6Vly1HAs8=' alt='error' width="100%" />
+        </div >
+    );
+};
+
+export default ErrorPage;
