50fe548bd58c737d990a0d14e1d60da6d15fcf2b
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-07-20 02:03:33 +0600

    Navigate with link component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,13 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const ErrorPage = () => {
+
+    const router = useRouter()
+
+    setTimeout(() => {
+        router.push('/')
+    }, 5000)
     return (
         <div>
             <img src='https://media.istockphoto.com/id/1142986365/vector/error-with-glitch-effect-on-screen-error-404-page-not-found-flat-design-modern-vector.jpg?s=612x612&w=0&k=20&c=E0Y1GZsaRrEk-a2eW49WKCjRFd9SS1MitZ6Vly1HAs8=' alt='error' width="100%" />
--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,9 +1,14 @@
+import { Button } from 'antd';
+import Link from 'next/link';
 import React from 'react';
 
 const ArticalPage = () => {
     return (
         <div>
-            <h1>This is articale</h1>
+            <h1>This is articale Home Page</h1>
+            <Button type='primary'>
+                <Link href="/">Home</Link >
+            </Button>
         </div>
     );
 };
